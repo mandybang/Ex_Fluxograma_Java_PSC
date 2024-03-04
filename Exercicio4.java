06d348d6271faf85f83f1626eb05133fb1a712fa
Amanda Fonseca <amandafj.estudante@gmail.com>
2024-03-04 20:52:51 -0300

    Exercicio atualizado

--- Exercicio4.java
+++ Exercicio4.java
@@ -1,6 +1,26 @@
-/**Crie um algoritmo que solicite o nome, o endereço e o telefone do usuário e 
+import java.util.Scanner;
+
+/**Crie um algoritmo que solicite o nome, o endereço e o telefone do usuário e
  * depois mostre os dados digitados.**/
 
 public class Exercicio4 {
-    
+     public static void main (String[]args) {
+        Scanner input = new Scanner(System.in);
+        String nome;
+        String end;
+        String num;
+
+        System.out.println("Digite seu nome: ");
+        nome = input.nextLine();
+
+        System.out.println("Digite seu endereço: ");
+        end = input.nextLine();
+
+        System.out.println("Digite seu telefone: ");
+        num = input.nextLine();
+
+        System.out.println("Seus dados são:" + nome +","+ end + ","+ num);
+ 
+        input.close();
+    }
 }
