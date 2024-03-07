fce885a77c605f49db1cde22dbf50c8274cbb869
Amanda Fonseca <amandafj.estudante@gmail.com>
2024-03-07 13:12:32 -0300

    Exercicio finalizado

---
+++ Exercicio17.java
@@ -0,0 +1,26 @@
+/**Faça um programa para o seguinte problema:
+ * Compraram-se N canetas iguais, que foram pagas com uma nota de Z reais, 
+ * obtendo-se Y reais como troco. Quanto custou cada caneta? */
+import java.util.Scanner;
+
+public class Exercicio17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Quantas canetas foram compradas? ");
+        int nCanetas = input.nextInt();
+
+        System.out.print("Quantos reais custaram as canetas? ");
+        double zReais = input.nextDouble();
+
+        System.out.print("Qual foi o troco recebido: ");
+        double yTroco = input.nextDouble();
+        
+        double custoTotal = zReais - yTroco;
+        double unitCaneta = custoTotal / nCanetas;
+        
+        System.out.println("Cada caneta custou: R$" + unitCaneta);
+        
+        input.close();
+    }
+}
